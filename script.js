7d133961b097ef690549eb0ad983976c34cc4524
jumpsq1227 <ogawa.ryohei.0622@gmail.com>
2026-01-23 17:33:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,22 @@ function runTraining() {
     return;
   }
 
+  switch (training) {
+    case "bench":
+      status.chest += 1;
+      break;
+    case "pullup":
+      status.back += 1;
+      break;
+    case "legpress":
+      status.leg += 1;
+      break;
+  }
   avatar.src = `images/${training}.png`;
   // avatar.src = `images/player_lv100.png`;
 
 }
+
 
 
 
