902ae3c8a82770428467c43f8eb4e2a337bd3f8d
jumpsq1227 <ogawa.ryohei.0622@gmail.com>
2026-01-24 09:18:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,59 @@
-let player = {
+// ステータス
+const status = {
+  run: 1,
   chest: 1,
   back: 1,
   leg: 1
 };
 
-// 初期ロード
-window.onload = () => {
-  const saved = localStorage.getItem("growfit_player");
-  if (saved) {
-    player = JSON.parse(saved);
+// 表示更新
+function updateStatusView() {
+  document.getElementById("HPLv").textContent = status.run;
+  document.getElementById("chestLv").textContent = status.chest;
+  document.getElementById("backLv").textContent = status.back;
+  document.getElementById("legLv").textContent = status.leg;
+}
+
+// ボタン実行
+function runTraining() {
+  const training = document.getElementById("training").value;
+  const avatar = document.getElementById("avatarImage");
+
+  if (!training) {
+    alert("筋トレ内容を選択してください");
+    return;
   }
-  updateStatus();
-};
 
-function updateStatus() {
-  document.getElementById("chestLv").textContent = player.chest;
-  document.getElementById("backLv").textContent = player.back;
-  document.getElementById("legLv").textContent = player.leg;
+  // ステータス、アバター更新
+  switch (training) {
+    case "run":
+      status.run += 1;
+      break;
+    case "chest":
+      status.chest += 1;
+      break;
+    case "back":
+      status.back += 1;
+      break;
+    case "leg":
+      status.leg += 1;
+      break;
+  }
+  updateStatusView();
+  avatar.src = `images/${training}.png`;
 }
 
-function startTraining() {
-  const type = document.getElementById("trainingType").value;
 
-  // レベルアップ
-  player[type]++;
-  localStorage.setItem("growfit_player", JSON.stringify(player));
 
-  // 画面切り替え
-  document.getElementById("main-screen").classList.add("hidden");
-  document.getElementById("result-screen").classList.remove("hidden");
 
-  // リザルト表示
-  const textMap = {
-    chest: "胸筋力 Lv UP！",
-    back: "背筋力 Lv UP！",
-    leg: "脚力 Lv UP！"
-  };
 
-  const resultText = document.getElementById("resultText");
-  resultText.textContent = textMap[type];
-  resultText.className = "level-up";
-}
 
-function backToMain() {
-  document.getElementById("result-screen").classList.add("hidden");
-  document.getElementById("main-screen").classList.remove("hidden");
-  updateStatus();
-}
+
+
+
+
+
+
+
+
+
